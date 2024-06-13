3affc39c94a62adbcf5e2dc58c6df2da04f26887
Billy Batista <bootlegbilly@protonmail.ch>
2024-06-12 20:50:21 -0400

    Add functions for encryption and getting user ID

--- biscuit-auth/src/elixir.rs
+++ biscuit-auth/src/elixir.rs
@@ -1,10 +1,11 @@
-use std::{
-    collections::{BTreeSet, HashMap},
-    iter::FromIterator,
-};
+use std::{collections::BTreeSet, iter::FromIterator, time::Duration};
+
+use chacha20poly1305::{aead::Aead, KeyInit, Nonce, XChaCha20Poly1305};
+use rustler::Binary;
 
 use crate::{
     builder::{fact, string, BiscuitBuilder, Fact, Term},
+    datalog::RunLimits,
     Authorizer, Biscuit, KeyPair, PrivateKey, PublicKey,
 };
 
@@ -53,6 +54,37 @@ fn generate(private_key: String, facts: Vec<(String, String, Vec<String>)>) -> S
 }
 
 #[rustler::nif]
+fn get_user_id(biscuit: String, public_key: String) -> i64 {
+    let public_key = PublicKey::from_bytes_hex(&public_key).unwrap();
+    let biscuit = Biscuit::from_base64(biscuit, public_key).unwrap();
+
+    let mut authorizer = Authorizer::new();
+    authorizer.add_token(&biscuit).unwrap();
+    authorizer
+        .add_code(
+            r#"
+        check if user($user);
+
+        allow if true;
+        deny if false;
+        "#,
+        )
+        .unwrap();
+
+    let user_info: Vec<(String,)> = authorizer
+        .query_with_limits(
+            "data($0) <- user($0)",
+            RunLimits {
+                max_time: Duration::from_secs(60),
+                ..Default::default()
+            },
+        )
+        .unwrap();
+
+    user_info.first().unwrap().0.parse().unwrap()
+}
+
+#[rustler::nif]
 fn authorize(biscuit: String, public_key: String, authorizer_code: String) -> bool {
     let public_key = PublicKey::from_bytes_hex(&public_key).unwrap();
     // This does verification for us as well, nice :)
@@ -63,12 +95,22 @@ fn authorize(biscuit: String, public_key: String, authorizer_code: String) -> bo
     authorizer.authorize().is_ok()
 }
 
+#[rustler::nif]
+fn encrypt(message: Binary, key: Binary, nonce: Binary) -> Result<Vec<u8>, String> {
+    let key = XChaCha20Poly1305::new_from_slice(key.as_slice()).map_err(|e| e.to_string())?;
+
+    key.encrypt(nonce.as_slice().into(), message.as_slice())
+        .map_err(|e| e.to_string())
+}
+
 rustler::init!(
     "Elixir.Biscuit",
     [
         new_private_key,
         generate,
         authorize,
-        public_key_from_private
+        public_key_from_private,
+        get_user_id,
+        encrypt,
     ]
 );
